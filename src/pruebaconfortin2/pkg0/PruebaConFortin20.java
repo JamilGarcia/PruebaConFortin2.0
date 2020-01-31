5b7e56c5022ee6e31ad010d7ef1a7b3b32c11dd0
JamilGarcia <you@example.com>
2020-01-31 10:16:24 -0600

    first commit

---
+++ src/pruebaconfortin2/pkg0/PruebaConFortin20.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaconfortin2.pkg0;
+
+/**
+ *
+ * @author JamilGarcía
+ */
+public class PruebaConFortin20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Simon");
+        // TODO code application logic here
+    }
+    
+}
